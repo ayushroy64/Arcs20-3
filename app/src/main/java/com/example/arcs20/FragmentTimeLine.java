fe7ab34738cb849942eb9afc56c65d0c50509fbe
AYUSH <ayushroy165@gmail.com>
2020-01-28 18:51:05 +0530

    commit 3

--- app/src/main/java/com/example/arcs20/FragmentTimeLine.java
+++ app/src/main/java/com/example/arcs20/FragmentTimeLine.java
@@ -4,16 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
 
-public class FragmentTimeLine  extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentTimeLine extends Fragment {
+
+    List<TimelineDataModel> timelineList;
+    ListView listView1, listView2, listView3;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_line, container, false);
+        View view= inflater.inflate(R.layout.fragment_time_line, container, false);
+
+        timelineList=new ArrayList<>();
+        listView1=view.findViewById(R.id.timeline_list_view_one);
+        listView2=view.findViewById(R.id.timeline_list_view_two);
+        listView3=view.findViewById(R.id.timeline_list_view_three);
+
+        timelineList.add(new TimelineDataModel(R.drawable.ic_rectangle,"12 pm", "UI/UX", "SJT301"));
+        timelineList.add(new TimelineDataModel(R.drawable.ic_rectangle,"12 pm", "UI/UX", "SJT301"));
+        timelineList.add(new TimelineDataModel(R.drawable.ic_rectangle,"12 pm", "UI/UX", "SJT301"));
+
+
+        DayOneAdapter adapter = new DayOneAdapter(getActivity(),R.layout.day_one_adapter,timelineList);
+
+        listView1.setAdapter(adapter);
+        listView2.setAdapter(adapter);
+        listView3.setAdapter(adapter);
+
+
+
+
+
+
+        return  view;
 
 
 
--- app/src/main/java/com/example/arcs20/MainActivity.java
+++ app/src/main/java/com/example/arcs20/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.arcs20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
