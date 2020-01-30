83d6112458ad1291266831176cff624cff6abba8
AYUSH <ayushroy165@gmail.com>
2020-01-30 08:28:41 +0530

    commit 4

--- app/src/main/java/com/example/arcs20/FragmentTimeLine.java
+++ app/src/main/java/com/example/arcs20/FragmentTimeLine.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class FragmentTimeLine extends Fragment {
 
-    List<TimelineDataModel> timelineList;
+    List<TimelineDataModel> timelineList,timelineList1;
     ListView listView1, listView2, listView3;
 
 
@@ -25,6 +25,7 @@ public class FragmentTimeLine extends Fragment {
         View view= inflater.inflate(R.layout.fragment_time_line, container, false);
 
         timelineList=new ArrayList<>();
+        timelineList1=new ArrayList<>();
         listView1=view.findViewById(R.id.timeline_list_view_one);
         listView2=view.findViewById(R.id.timeline_list_view_two);
         listView3=view.findViewById(R.id.timeline_list_view_three);
@@ -34,11 +35,20 @@ public class FragmentTimeLine extends Fragment {
         timelineList.add(new TimelineDataModel(R.drawable.ic_rectangle,"12 pm", "UI/UX", "SJT301"));
 
 
+        timelineList1.add(new TimelineDataModel(R.drawable.ic_rectangle,"12 pm", "UI/UX", "SJT302"));
+        timelineList1.add(new TimelineDataModel(R.drawable.ic_rectangle,"12 pm", "UI/UX", "SJT302"));
+        timelineList1.add(new TimelineDataModel(R.drawable.ic_rectangle,"12 pm", "UI/UX", "SJT302"));
+
+
         DayOneAdapter adapter = new DayOneAdapter(getActivity(),R.layout.day_one_adapter,timelineList);
+        DayTwoAdapter adapter1 = new DayTwoAdapter(getActivity(),R.layout.day_one_adapter,timelineList1);
 
         listView1.setAdapter(adapter);
-        listView2.setAdapter(adapter);
-        listView3.setAdapter(adapter);
+        listView1.setAdapter(adapter);
+        listView1.setAdapter(adapter);
+        listView2.setAdapter(adapter1);
+        listView3.setAdapter(adapter1);
+        listView3.setAdapter(adapter1);
 
 
 
--- app/src/main/java/com/example/arcs20/MainActivity.java
+++ app/src/main/java/com/example/arcs20/MainActivity.java
@@ -6,13 +6,27 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // Create new fragment and transaction
+        Fragment newFragment = new FragmentTimeLine();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+        transaction.replace(R.id.fragmentView, newFragment);
+        //transaction.addToBackStack(null);
+
+// Commit the transaction
+        transaction.commit();
 
     }
 }
